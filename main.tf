51cff3681169f0fcbb8eb75e1cebbca6907cf8d0
XIC918-smoral <101389596+XIC918-smoral@users.noreply.github.com>
2023-03-02 09:12:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+#   access_key = var.access_key
+#   secret_key = var.secret_key
 }
 
-variable "access_key" {
-}
-variable "secret_key" {
-}
+# variable "access_key" {
+# }
+# variable "secret_key" {
+# }
 
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
