ea936dad770c20f523a7b4ade56e1f4af914d3b8
XIC918-smoral <101389596+XIC918-smoral@users.noreply.github.com>
2023-03-02 10:38:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ resource "aws_db_instance" "default" {
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   publicly_accessible  = true
-  username             = "foo"
-  password             = "foobarbaz"
+  username             = var.user
+  password             = var.password
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
