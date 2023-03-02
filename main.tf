47123c09ddb32644f9e0e0702334fd1e30c0b7eb
XIC918-smoral <101389596+XIC918-smoral@users.noreply.github.com>
2023-03-02 09:04:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 provider "aws" {
   region = "ap-south-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+variable "access_key" {
+}
+varible "secret_key" {
 }
 
 resource "aws_db_instance" "default" {
